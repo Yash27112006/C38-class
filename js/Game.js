b4479aa3b96897377660554ac491bca9effaaf5a
Yash27112006 <yashu53421@gmail.com>
2020-06-07 13:53:25 +0530

    Car racing Win

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("car1",car1Img);
     car2 = createSprite(300,200);
+    car2.addImage("car2",car2Img);
     car3 = createSprite(500,200);
+    car3.addImage("car3",car3Img);
     car4 = createSprite(700,200);
+    car4.addImage("car4",car4Img);
     cars = [car1, car2, car3, car4];
   }
 
@@ -43,18 +47,20 @@ class Game {
     
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+      background("brown");
+      image(trackImg,0,-displayHeight*4,displayWidth,displayHeight*5);
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
         //add 1 to the index for every loop
         index = index + 1 ;
-
+      //  console.log(plr);
+      // console.log("allPlayers" + allPlayers);
         //position the cars a little away from each other in x direction
         x = x + 200;
         //use data form the database to display the cars in y direction
@@ -78,7 +84,13 @@ class Game {
       player.distance +=10
       player.update();
     }
+    if(player.distance>3590){
+      gameState = 2;
+    }
 
     drawSprites();
+  }
+  end(){
+    console.log("gameEnded")
   }
 }
--- sketch.js
+++ sketch.js
@@ -9,8 +9,15 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var car1Img,car2Img,car3Img,car4Img,trackImg;
 
-
+function preload(){
+  car1Img = loadImage("images/car1.png");
+  car2Img = loadImage("images/car2.png");
+  car3Img = loadImage("images/car3.png");
+  car4Img = loadImage("images/car4.png");
+  trackImg = loadImage("images/track.png");
+}
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
@@ -19,7 +26,6 @@ function setup(){
   game.start();
 }
 
-
 function draw(){
   if(playerCount === 4){
     game.update(1);
@@ -27,5 +33,9 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.end();
+    game.update(2);
   }
 }
